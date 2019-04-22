d0b0e0aede0866791f15126458a368b81b6d60c6
Thiekus <thekill96@gmail.com>
2019-04-22 19:16:51 +0700

    Minor fixes under admin panel

--- appmain.go
+++ appmain.go
@@ -8,14 +8,14 @@ import (
 	"github.com/gorilla/sessions"
 	sqlite "github.com/mattn/go-sqlite3"
 	"net/http"
-	_ "net/http/pprof"
 	"os"
 	"runtime"
 	"strings"
 	"time"
 )
 
-const appVersion = "0.1"
+const appVersion = "0.1.1"
+
 var appConfig ConfigData
 var appServer http.Server
 var appServerVer = fmt.Sprintf("ThkTwinkle %s", appVersion)
@@ -26,12 +26,12 @@ var appCookieStore *sessions.CookieStore
 func shutdownEndpoint(w http.ResponseWriter, r *http.Request) {
 	// Check authentication before do shutdown
 	if checkAdminAuth(r) {
-		log:= newLog()
+		log := newLog()
 		log.Print("Requesting shutdown...")
-		go func(){
+		go func() {
 			time.Sleep(5000 * time.Millisecond)
 			log.Print("Shutting down...")
-			if err:= appServer.Shutdown(context.Background()); err != nil {
+			if err := appServer.Shutdown(context.Background()); err != nil {
 				log.Printf("Shutdown error: %s", err)
 			}
 		}()
@@ -44,7 +44,7 @@ func shutdownEndpoint(w http.ResponseWriter, r *http.Request) {
 // Main middleware, invoking some tweaks
 func appMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log:= newLog()
+		log := newLog()
 		if !strings.HasPrefix(r.URL.Path, "/importStatus") {
 			log.Printf("Client %s accessing %s", r.RemoteAddr, r.URL.Path)
 		}
@@ -59,26 +59,26 @@ func appMiddleware(next http.Handler) http.Handler {
 func main() {
 	fmt.Printf("Thiekus IMDb Movies WebService codename Twinkle v%s\n", appVersion)
 	fmt.Println("Copyright (C) Thiekus 2019")
-	sqliteVer, _, _:= sqlite.Version()
+	sqliteVer, _, _ := sqlite.Version()
 	fmt.Printf("Built using %s, SQLite %s\n\n", runtime.Version(), sqliteVer)
 
 	// Easter egg
-	now:= time.Now()
-	if (now.Hour() >= 0) && (now.Hour() < 5){
-		hideData:= "TXVuZ2tpbiBoYW55YSBvcmFuZyBnaWxhIGRhbiBrdXJhbmcga2VyamFhbiBiaWtpbiBhcGxpa2FzaSBrYXlhayBnaW5pLCBkaSBqYW0gc2VnaW5pIDop"
-		hideStr, _:= base64.StdEncoding.DecodeString(hideData)
+	now := time.Now()
+	if (now.Hour() >= 0) && (now.Hour() < 5) {
+		hideData := "TXVuZ2tpbiBoYW55YSBvcmFuZyBnaWxhIGRhbiBrdXJhbmcga2VyamFhbiBiaWtpbiBhcGxpa2FzaSBrYXlhayBnaW5pLCBkaSBqYW0gc2VnaW5pIDop"
+		hideStr, _ := base64.StdEncoding.DecodeString(hideData)
 		fmt.Printf("%s\n\n", hideStr)
 	}
-	log:= newLog()
+	log := newLog()
 
 	// Get configuration data
 	appConfig = getConfigData()
 	appCookieStore = sessions.NewCookieStore([]byte(appConfig.SessionKey))
 
 	// Initialize cache dir if not available
-	cacheDir:= "./caches"
+	cacheDir := "./caches"
 	if !isFileExists(cacheDir) {
-		if err:= os.Mkdir(cacheDir, os.ModePerm); err == nil {
+		if err := os.Mkdir(cacheDir, os.ModePerm); err == nil {
 			log.Printf("Cache directory created at %s", cacheDir)
 		} else {
 			panic(err)
@@ -86,7 +86,7 @@ func main() {
 	}
 
 	// Define our webservice routing
-	r:= mux.NewRouter()
+	r := mux.NewRouter()
 	r.Use(appMiddleware)
 	// Main webservice endpoints
 	r.HandleFunc("/movies", moviesGetEndpoint).Methods("GET")
@@ -108,7 +108,7 @@ func main() {
 
 	// Establish our server
 	appServer = http.Server{Addr: fmt.Sprintf(":%d", appConfig.ListeningPort), Handler: r}
-	if err:= appServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+	if err := appServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		panic(err)
 	}
 }
