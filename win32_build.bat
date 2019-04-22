22c7026109466f0cb07cd3a25dda9c522995ff85
Thiekus <thekill96@gmail.com>
2019-04-22 19:41:27 +0700

    Additional go linker flags to remove debugging info + shrink size

--- win32_build.bat
+++ win32_build.bat
@@ -3,5 +3,5 @@ set GOROOT=C:\Go32
 set GOARCH=386
 set PATH=C:\Go32\bin;C:\mingw81_32\bin;%PATH%
 echo Building, please wait...
-go build -i -v -o imdbws32.exe
+go build -i -v -ldflags="-s -w" -o imdbws32.exe
 pause
--- win64_build.bat
+++ win64_build.bat
@@ -2,5 +2,5 @@
 set GOARCH=amd64
 set PATH=C:\mingw81_64\bin;%PATH%
 echo Building, please wait...
-go build -i -v -o imdbws64.exe
+go build -i -v -ldflags="-s -w" -o imdbws64.exe
 pause
